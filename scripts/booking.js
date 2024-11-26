1fc703f34f0eae99f98c4c285e84d10cce5b006d
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2024-11-26 13:24:59 -0700

    Worked on booking.js

--- scripts/booking.js
+++ scripts/booking.js
@@ -2,16 +2,15 @@
 // useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
-
+let cost = 0
+let day = ""
+let hours = "full"
 
 
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
-
-
-
 
 
 /********* clear days *********/
