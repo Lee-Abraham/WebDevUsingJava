96ee361b2fece218dfdf129a0af5f4f9396dd7a8
Kiwito <lyubstriletskyy@gmail.com>
2024-12-04 12:41:15 -0700

    removed the preventdefault

--- scripts/booking.js
+++ scripts/booking.js
@@ -4,9 +4,8 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 let cost = 0;
-cost.preventDefault();
 let selected = "full";
-selected.preventdefault();
+
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
@@ -45,7 +44,7 @@ ClearButton.addEventListener('click', clearDays);
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 const halfs = document.getElementById("half");
-halfs.addEventListener('click', (e) => {
+halfs.addEventListener('click', () => {
     e.target.classList.add("clicked");
     fulls.classList.remove("clicked");
 });
@@ -54,7 +53,7 @@ halfs.addEventListener('click', (e) => {
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 const fulls = document.getElementById("full");
-fulls.addEventListener('click', (e) => {
+fulls.addEventListener('click', () => {
     e.target.classList.add("clicked");
     halfs.classList.remove("clicked");
 });
@@ -65,13 +64,11 @@ fulls.addEventListener('click', (e) => {
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
 const money = document.getElementById("calculated-cost");
-halfs.addEventListener('click', (e) => {
+halfs.addEventListener('click', () => {
     money.innerHTML = "20";
-    e.preventDefault();
 });
-fulls.addEventListener("click", (e) => {
+fulls.addEventListener("click", () => {
     money.innerHTML = "35";
-    e.preventDefault();
 });
 
 
