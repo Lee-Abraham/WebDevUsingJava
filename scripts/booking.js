9fe4ed4a56f7441539439dd7fdee36a07bc1539e
Kiwito <lyubstriletskyy@gmail.com>
2024-12-04 12:35:22 -0700

    changed a bunch of syntax errors

--- scripts/booking.js
+++ scripts/booking.js
@@ -4,8 +4,9 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 let cost = 0;
+cost.preventDefault();
 let selected = "full";
-
+selected.preventdefault();
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
@@ -14,9 +15,9 @@ const dayButtons = document.querySelectorAll('.day-selector > li');
 dayButtons.forEach(dayButton => {
     dayButton.addEventListener('click', (e) => {
         dayButtons.forEach(dayButton => {
-            dayButton.classList.remove('clicked')
-        })
-        e.target.classList.add('clicked')
+            dayButton.classList.remove('clicked');
+        });
+        e.target.classList.add('clicked');
     });
 });
 
@@ -43,7 +44,7 @@ ClearButton.addEventListener('click', clearDays);
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
-const halfs = document.getElementById("half")
+const halfs = document.getElementById("half");
 halfs.addEventListener('click', (e) => {
     e.target.classList.add("clicked");
     fulls.classList.remove("clicked");
@@ -52,7 +53,7 @@ halfs.addEventListener('click', (e) => {
 
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
-const fulls = document.getElementById("full")
+const fulls = document.getElementById("full");
 fulls.addEventListener('click', (e) => {
     e.target.classList.add("clicked");
     halfs.classList.remove("clicked");
@@ -63,12 +64,14 @@ fulls.addEventListener('click', (e) => {
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
-const money = document.getElementById("calculated-cost")
+const money = document.getElementById("calculated-cost");
 halfs.addEventListener('click', (e) => {
     money.innerHTML = "20";
+    e.preventDefault();
 });
 fulls.addEventListener("click", (e) => {
     money.innerHTML = "35";
+    e.preventDefault();
 });
 
 
