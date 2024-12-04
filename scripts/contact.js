1fcf8bcedf3a766b8447240d733e3dc31289a9ce
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2024-12-04 12:41:22 -0700

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,7 +1,7 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 document.getElementById("submit-button").addEventListener("click", thank)
 function thank() {
-    document.getElementById("contact-page").innerHTML =  "Thank you for you message"
+    document.getElementById("contact-page").innerHTML =  "Thank you for you message";
     lee.style.fontSize = "24px";
 }
 const lee = document.getElementById("contact-page")
